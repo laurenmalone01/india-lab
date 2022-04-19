83516d0eb1c5e1a4258b166d628f13ae314bcd97
Lauren Malone <97190412+laurenmalone01@users.noreply.github.com>
2022-04-19 12:05:02 -0500

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,59 @@
+$(function(){
+    // $("#submitButton").click(function () {
+        
+    //   });
+
+});//end of doc
+    // let genres = [
+    //     ["diamond", ["Pop", "Rock", "Rap", "R&B"]],
+    //     ["moon", ["Pop", "Rock", "Rap", "R&B"]],
+    //     ["star", ["Pop", "Rock", "Rap", "R&B"]],
+    //     ["fox", ["Pop", "Rock", "Rap", "R&B"]]
+    // ];
+
+
+    // $("#emojiKind").on("change", function(e) {
+    //     //enables the genre dropdown
+    //     $("#genre").prop("disabled", false);
+
+    //     let inputVal = this.value;
+    //     // or $("#petkind").value?
+    //     // console.log(inputVal);
+
+    //     //loop though array of genres
+    //     $.each(genres, function(key, value) {
+    //         //match genre to user selected
+    //         if (inputVal === value[0]) {
+    //             console.log("value[0]: +" + value[0] + ", key: " + key + "value: " + value);
+    //             $.each(value, function(nestKey, nestValue) {
+    //                 // console.log(nestKey);
+
+    //                 switch (nestKey) {
+    //                     case 0:
+    //                         $("label[for=genre]").text(nestValue);
+    //                         $("#genre").empty();
+    //                         $("#genre").append(
+    //                             $("<option>").text(`select a ${nestValue} `)
+    //                         );
+    //                         break;
+    //                     case 1:
+    //                         $.each(nestValue, function(nameKey, nameValue) {
+    //                             console.log(nameKey, nameValue);
+
+    //                             $("#genre").append(
+    //                                 $("<option>").val(nameValue).text(nameValue)
+    //                             );
+    //                         });
+    //                         break;
+    //                 }
+    //             });
+    //         }
+    //     });
+
+    //     // $("#submitButton").
+    //     // //prevent default
+    // });
+
+    // $("#clear").click(() => {
+    //     form.reset();
+    //   });
