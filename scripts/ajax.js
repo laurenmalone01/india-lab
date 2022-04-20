ddd31886b413a00ad29f6a18125213f564f2790b
Lauren Malone <97190412+laurenmalone01@users.noreply.github.com>
2022-04-19 22:20:47 -0500

    Add files via upload

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -3,27 +3,14 @@
 
     $('#btnLoadData').click(function() {
         console.log("clicked");
-    
-    //     let jsonURL = "https://www.w3schools.com/jquery/demo_ajax_json.js";
-    
+        
         let jsonURL = "demo.json";
     
-        // let jsonURL = "https://barrycumbie.github.io/376-india-lab/demo.json";
     
         $.ajax({
             url: jsonURL,
             dataType: "json",
             success: function(data) {
-                //can log either the entire data or invoke specific properties data.zipCode...
-                // console.log(data.firstName);
-                
-                //loads first name into my first input box (in example #1)
-                // $("#noSpaces").val(data.firstName);
-                // console.log(data.lastName);
-                // console.log(data.email);
-                // console.log(data.password);
-                // console.log(data.tempoType);
-
 
                 $('input[type=radio]').each(function () {
                     if (data.tempoType === $(this).val())
@@ -34,23 +21,12 @@
     
                 });
 
-                // $('input[type=radio]').each(function () {
-                //     if (data.tempoType === $(this).val())
-                //     {
-                //       $(this).prop('checked', true)
-                //         console.log($(this));
-                //    }
-    
-                // });
-
-                // console.log (sList);
-    
+            
                 $.each(data, function (key, val) {
                     //step through results
                     console.log(key, val);
                     //get each data item
                     //put it in the right place
-                    //$("#dataContainer").append(key + " " + val);
                     $(`#${key}`).val(val);
                     
                 });
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,59 +1,21 @@
 $(function(){
+    $("#clear").click(() => {
+        form.reset();
+      });
+
+        var old = console.log;
+        var logger = document.getElementById('log');
+        console.log = function (message) {
+            if (typeof message == 'object') {
+                logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+            } else {
+                logger.innerHTML += message + '<br />';
+            }
+        }
+    ();
     // $("#submitButton").click(function () {
         
     //   });
 
 });//end of doc
-    // let genres = [
-    //     ["diamond", ["Pop", "Rock", "Rap", "R&B"]],
-    //     ["moon", ["Pop", "Rock", "Rap", "R&B"]],
-    //     ["star", ["Pop", "Rock", "Rap", "R&B"]],
-    //     ["fox", ["Pop", "Rock", "Rap", "R&B"]]
-    // ];
-
-
-    // $("#emojiKind").on("change", function(e) {
-    //     //enables the genre dropdown
-    //     $("#genre").prop("disabled", false);
-
-    //     let inputVal = this.value;
-    //     // or $("#petkind").value?
-    //     // console.log(inputVal);
-
-    //     //loop though array of genres
-    //     $.each(genres, function(key, value) {
-    //         //match genre to user selected
-    //         if (inputVal === value[0]) {
-    //             console.log("value[0]: +" + value[0] + ", key: " + key + "value: " + value);
-    //             $.each(value, function(nestKey, nestValue) {
-    //                 // console.log(nestKey);
-
-    //                 switch (nestKey) {
-    //                     case 0:
-    //                         $("label[for=genre]").text(nestValue);
-    //                         $("#genre").empty();
-    //                         $("#genre").append(
-    //                             $("<option>").text(`select a ${nestValue} `)
-    //                         );
-    //                         break;
-    //                     case 1:
-    //                         $.each(nestValue, function(nameKey, nameValue) {
-    //                             console.log(nameKey, nameValue);
-
-    //                             $("#genre").append(
-    //                                 $("<option>").val(nameValue).text(nameValue)
-    //                             );
-    //                         });
-    //                         break;
-    //                 }
-    //             });
-    //         }
-    //     });
-
-    //     // $("#submitButton").
-    //     // //prevent default
-    // });
-
-    // $("#clear").click(() => {
-    //     form.reset();
-    //   });
+   
